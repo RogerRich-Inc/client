6233afa8ffa8ad0d823bd70e51a00914a5ccf5b9
RogelioRichmanAstronaut <56854119+RogelioRichmanAstronaut@users.noreply.github.com>
2021-12-29 23:34:49 -0500

    v2

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react"
 import styled from "styled-components"
 import { popularProducts } from "../data"
 import Product from "./Product"
-import axios from "axios"
+//import axios from "axios"
+import { publicRequest } from "../requestMethods"
 
 const Container = styled.div`
  padding: 20px;
@@ -18,10 +19,10 @@ const Products = ({cat,filters,sort}) => {
     useEffect(()=>{
         const getProducts = async ()=>{
             try{
-                const res = await axios.get(
+                const res = await publicRequest.get(
                    cat 
-                     ? `http://localhost:5000/api/products?category=${cat}` 
-                     : "http://localhost:5000/api/products"
+                     ? `/products?category=${cat}` 
+                     : "/api/products"
                 );
                 setProducts(res.data);
             }catch(err){}
